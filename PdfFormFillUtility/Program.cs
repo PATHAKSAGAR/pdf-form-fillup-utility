35ed046387e076e8e2bb8d20572e035d04c03b4b
Sagar Pathak <sagar18889@outlook.com>
2022-05-23 12:31:07 +0530

    Initial Commit

---
+++ PdfFormFillUtility/Program.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using PdfFormFillUtility.Utility;
+
+namespace PdfFormFillUtility
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"C:/Sagar/Learnings/Conosle Programs/leaveform.pdf";
+            var dataModel = new Model()
+            {
+                FullName = "Shivaansh Pathak",
+                FamilyName = "Sagar Pathak",
+                Language1 = "Off",
+                Language2 = "Off",
+                Language3 = "Off",
+                Language4 = "Off",
+                Language5 = "On",
+                FavColour = "Orange",
+                Address1 = "JB Nagar",
+                Address2 = "Chakala, Andheri East",
+                HouseNumber = "23 A",
+                City = "Akola",
+                PostalCode = "12345",
+                Country = "Denmark",
+                Gender = "Man",
+                Height = "120",
+                DrivingLicense = "On"
+            };
+
+            var data = PdfFormUtility<Model>.Generate(filePath, JsonConvert.SerializeObject(dataModel));
+            //var data = PdfFormUtility<Model>.Generate(filePath, dataModel);
+            System.IO.File.WriteAllBytes("C:/Sagar/Learnings/Conosle Programs/hello.pdf", data);
+        }
+
+        public class Model
+        {
+            [JsonProperty(PropertyName = "Given Name Text Box")]
+            public string FullName { get; set; }
+            [JsonProperty(PropertyName = "Family Name Text Box")]
+            public string FamilyName { get; set; }
+            [JsonProperty(PropertyName = "Favourite Colour List Box")]
+            public string FavColour { get; set; }
+            [JsonProperty(PropertyName = "Language 1 Check Box")]
+            public string Language1 { get; set; }
+            [JsonProperty(PropertyName = "Language 2 Check Box")]
+            public string Language2 { get; set; }
+            [JsonProperty(PropertyName = "Language 3 Check Box")]
+            public string Language3 { get; set; }
+            [JsonProperty(PropertyName = "Language 4 Check Box")]
+            public string Language4 { get; set; }
+            [JsonProperty(PropertyName = "Language 5 Check Box")]
+            public string Language5 { get; set; }
+            [JsonProperty(PropertyName = "Address 1 Text Box")]
+            public string Address1 { get; set; }
+            [JsonProperty(PropertyName = "Address 2 Text Box")]
+            public string Address2 { get; set; }
+            [JsonProperty(PropertyName = "Postcode Text Box")]
+            public string PostalCode { get; set; }
+            [JsonProperty(PropertyName = "City Text Box")]
+            public string City { get; set; }
+            [JsonProperty(PropertyName = "House nr Text Box")]
+            public string HouseNumber { get; set; }
+            [JsonProperty(PropertyName = "Country Combo Box")]
+            public string Country { get; set; }
+            [JsonProperty(PropertyName = "Gender List Box")]
+            public string Gender { get; set; }
+            [JsonProperty(PropertyName = "Height Formatted Field")]
+            public string Height { get; set; }
+            [JsonProperty(PropertyName = "Driving License Check Box")]
+            public string DrivingLicense { get; set; }
+        }
+    }
+}
---
+++ PdfFormUtility/Utility/PdfFormUtility.cs
@@ -0,0 +1,116 @@
+﻿using iText.Forms;
+using iText.Kernel.Pdf;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace PdfFormFillUtility.Utility
+{
+    /// <summary>
+    /// T should be a class where properties should be same as pdf feilds names or you can also use attibute mapping to set custom name to property.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class PdfFormUtility<T> where T : class
+    {
+        private static Dictionary<string, string> GetMappings(T model)
+        {
+            Dictionary<string, string> mappings = new Dictionary<string, string>();
+
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                var customName = prop.GetCustomAttribute<JsonPropertyAttribute>().PropertyName;
+                mappings.Add(string.IsNullOrWhiteSpace(customName) ? prop.Name : customName, prop.GetValue(model).ToString());
+            }
+
+            return mappings;
+        }
+
+        /// <summary>
+        /// Return pdf bytes which can be used to create pdf.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="dataModel"></param>
+        /// <returns></returns>
+        public static byte[]? Generate(string filePath, T dataModel)
+        {
+            var dictMapper = GetMappings(dataModel);
+            using var outMS = new MemoryStream();
+            var reader = new PdfReader(filePath);
+            var pdfDoc = new PdfDocument(reader, new PdfWriter(outMS));
+            if (dataModel != null)
+            {
+                FillForm(pdfDoc, dictMapper);
+
+                pdfDoc.Close();
+
+                return outMS.ToArray();
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public static byte[]? Generate(string filePath, string dataModel)
+        {
+            var dictMapper = JsonConvert.DeserializeObject<Dictionary<string, string>>(dataModel);
+            using var outMS = new MemoryStream();
+            var reader = new PdfReader(filePath);
+            var pdfDoc = new PdfDocument(reader, new PdfWriter(outMS));
+            if (dataModel != null)
+            {
+                FillForm(pdfDoc, dictMapper);
+
+                pdfDoc.Close();
+
+                return outMS.ToArray();
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        private static void FillForm(PdfDocument pdfDoc, Dictionary<string, string> dictMapper)
+        {
+            var form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+            foreach (var item in form.GetFormFields())
+            {
+                item.Value.SetReadOnly(false);
+                if (dictMapper.TryGetValue(item.Key, out string value))
+                {
+                    item.Value.SetValue(value);
+                }
+                item.Value.SetReadOnly(true);
+            }
+        }
+
+        public static object GetFeildsToMap(string filePath)
+        {
+            dynamic expando = new ExpandoObject();
+            using var outMS = new MemoryStream();
+            var reader = new PdfReader(filePath);
+            var pdfDoc = new PdfDocument(reader, new PdfWriter(outMS));
+            var form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+            var feilds = form.GetFormFields().Select(i => i.Key).ToList();
+            feilds.ForEach(i =>
+            {
+                AddProperty(expando, i.Replace(" ", "_"), string.Empty);
+            });
+            return expando;
+        }
+
+        public static void AddProperty(ExpandoObject expando, string propertyName, object propertyValue)
+        {
+            // ExpandoObject supports IDictionary so we can extend it like this
+            var expandoDict = expando as IDictionary<string, object>;
+            if (expandoDict.ContainsKey(propertyName))
+                expandoDict[propertyName] = propertyValue;
+            else
+                expandoDict.Add(propertyName, propertyValue);
+        }
+    }
+}
